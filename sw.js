6feb94589a634c560fc41fb28867a34e21ce6ca2
nancydavapp <nancy.aguilar.verdugonav@gmail.com>
2026-04-08 19:34:51 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'terrazul-v1';
+const URLS_TO_CACHE = [
+  '/Terrazul/terrazul-actividades.html',
+  '/Terrazul/manifest.json',
+  '/Terrazul/icon192.png',
+  '/Terrazul/icon512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') return response;
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseToCache));
+        return response;
+      });
+    }).catch(() => caches.match('/Terrazul/terrazul-actividades.html'))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
